f74327980a0ea99493f52e4abf6a4c160e71757b
Ivochkina <romina23082008@gmail.com>
2021-01-05 13:58:59 +0200

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,56 @@
+'use strict'
+
+let hi = 'hello';
+let o = 'owu';
+let web = 'com';
+let country = 'ua';
+
+console.log(hi);
+console.log(o);
+console.log(web);
+console.log(country);
+
+alert(hi, '');
+alert(o, '');
+alert(web, '');
+alert(country, '');
+//
+// document.write(hi);
+// document.write(o, "/br");
+// document.write(web);
+// document.write(country);
+
+
+let day = 1;
+let mounth = 10;
+let age = -999;
+let num = 123;
+let juf = 3.14;
+let kfo = 16;
+
+
+console.log(day);
+console.log(mounth);
+console.log(age);
+console.log(num);
+console.log(juf);
+console.log(kfo);
+
+alert(day, '');
+alert(mounth, '');
+alert(age, '');
+alert(num, '');
+alert(juf, '');
+alert(kfo, '');
+
+// document.write(hi);
+// document.write(o);
+// document.write(web);
+// document.write(country);
+
+let iiii = true;
+let llll = false;
+
+
+
+
