d8115db8d1aa21fa70d26f27bfd5f7fef2297e7d
Ivochkina <romina23082008@gmail.com>
2021-01-05 14:32:28 +0200

    create 1 task

--- js/scripts.js
+++ js/scripts.js
@@ -1,66 +1,66 @@
-// 'use strict'
-//
-// let mess = 'hello';
-// let test1 = 'owu';
-// let web = 'com';
-// let country = 'ua';
-//
-// console.log(mess);
-// console.log(test1);
-// console.log(web);
-// console.log(country);
-//
-// alert(mess, '');
-// alert(test1, '');
-// alert(web, '');
-// alert(country, '');
-//
-// document.write(mess + '<br>');
-// document.write(test1 + '<br>');
-// document.write(web + '<br>');
-// document.write(country +'<br>');
-//
-//
-// let day = 1;
-// let mounth = 10;
-// let num = -999;
-// let test2 = 123;
-// let pi = 3.14;
-// let age = 16;
-//
-//
-// console.log(day);
-// console.log(mounth);
-// console.log(num);
-// console.log(test2);
-// console.log(pi);
-// console.log(age);
-//
-// alert(day, '');
-// alert(mounth, '');
-// alert(num, '');
-// alert(test2, '');
-// alert(pi, '');
-// alert(age, '');
-//
-// document.write(day +'<br>');
-// document.write(mounth +'<br>');
-// document.write(num +'<br>');
-// document.write(test2 +'<br>');
-// document.write(pi +'<br>');
-// document.write(age +'<br>');
-//
-// let isYes = true;
-// let isNo= false;
-//
-// console.log(isYes);
-// console.log(isNo);
-//
-// alert(isYes,'');
-// alert(isNo,'');
-//
-// document.write(isYes +'<br>');
-// document.write(isNo +'<br>');
-//
-//
-//
+'use strict'
+
+let mess = 'hello';
+let test1 = 'owu';
+let web = 'com';
+let country = 'ua';
+
+console.log(mess);
+console.log(test1);
+console.log(web);
+console.log(country);
+
+alert(mess, '');
+alert(test1, '');
+alert(web, '');
+alert(country, '');
+
+document.write(mess + '<br>');
+document.write(test1 + '<br>');
+document.write(web + '<br>');
+document.write(country +'<br>');
+
+
+let day = 1;
+let mounth = 10;
+let num = -999;
+let test2 = 123;
+let pi = 3.14;
+let age = 16;
+
+
+console.log(day);
+console.log(mounth);
+console.log(num);
+console.log(test2);
+console.log(pi);
+console.log(age);
+
+alert(day, '');
+alert(mounth, '');
+alert(num, '');
+alert(test2, '');
+alert(pi, '');
+alert(age, '');
+
+document.write(day +'<br>');
+document.write(mounth +'<br>');
+document.write(num +'<br>');
+document.write(test2 +'<br>');
+document.write(pi +'<br>');
+document.write(age +'<br>');
+
+let isYes = true;
+let isNo= false;
+
+console.log(isYes);
+console.log(isNo);
+
+alert(isYes,'');
+alert(isNo,'');
+
+document.write(isYes +'<br>');
+document.write(isNo +'<br>');
+
+
+
