2b5b8aafe054820f56a1a163d1b2b78ff3d28146
Ivochkina <romina23082008@gmail.com>
2021-01-05 14:30:02 +0200

    test

--- js/scripts.js
+++ js/scripts.js
@@ -1,56 +1,66 @@
-'use strict'
-
-let hi = 'hello';
-let o = 'owu';
-let web = 'com';
-let country = 'ua';
-
-console.log(hi);
-console.log(o);
-console.log(web);
-console.log(country);
-
-alert(hi, '');
-alert(o, '');
-alert(web, '');
-alert(country, '');
+// 'use strict'
 //
-// document.write(hi);
-// document.write(o, "/br");
-// document.write(web);
-// document.write(country);
-
-
-let day = 1;
-let mounth = 10;
-let age = -999;
-let num = 123;
-let juf = 3.14;
-let kfo = 16;
-
-
-console.log(day);
-console.log(mounth);
-console.log(age);
-console.log(num);
-console.log(juf);
-console.log(kfo);
-
-alert(day, '');
-alert(mounth, '');
-alert(age, '');
-alert(num, '');
-alert(juf, '');
-alert(kfo, '');
-
-// document.write(hi);
-// document.write(o);
-// document.write(web);
-// document.write(country);
-
-let iiii = true;
-let llll = false;
-
-
-
-
+// let mess = 'hello';
+// let test1 = 'owu';
+// let web = 'com';
+// let country = 'ua';
+//
+// console.log(mess);
+// console.log(test1);
+// console.log(web);
+// console.log(country);
+//
+// alert(mess, '');
+// alert(test1, '');
+// alert(web, '');
+// alert(country, '');
+//
+// document.write(mess + '<br>');
+// document.write(test1 + '<br>');
+// document.write(web + '<br>');
+// document.write(country +'<br>');
+//
+//
+// let day = 1;
+// let mounth = 10;
+// let num = -999;
+// let test2 = 123;
+// let pi = 3.14;
+// let age = 16;
+//
+//
+// console.log(day);
+// console.log(mounth);
+// console.log(num);
+// console.log(test2);
+// console.log(pi);
+// console.log(age);
+//
+// alert(day, '');
+// alert(mounth, '');
+// alert(num, '');
+// alert(test2, '');
+// alert(pi, '');
+// alert(age, '');
+//
+// document.write(day +'<br>');
+// document.write(mounth +'<br>');
+// document.write(num +'<br>');
+// document.write(test2 +'<br>');
+// document.write(pi +'<br>');
+// document.write(age +'<br>');
+//
+// let isYes = true;
+// let isNo= false;
+//
+// console.log(isYes);
+// console.log(isNo);
+//
+// alert(isYes,'');
+// alert(isNo,'');
+//
+// document.write(isYes +'<br>');
+// document.write(isNo +'<br>');
+//
+//
+//
