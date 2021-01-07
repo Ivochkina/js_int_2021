7323e14be1e459770e23083112efb8d9c1f48a3e
Ivochkina <romina23082008@gmail.com>
2021-01-07 14:02:48 +0200

    created new file task_Class

--- js/scripts.js
+++ js/scripts.js
@@ -1,369 +1,377 @@
-'use strict'
-
-
-// task 1
-let mess = 'hello';
-let test1 = 'owu';
-let web = 'com';
-let country = 'ua';
-
-console.log(mess);
-console.log(test1);
-console.log(web);
-console.log(country);
-
-alert(mess);
-alert(test1);
-alert(web);
-alert(country);
-
-document.write(mess + '<br>');
-document.write(test1 + '<br>');
-document.write(web + '<br>');
-document.write(country +'<br>');
-
-
-let day = 1;
-let mounth = 10;
-let num = -999;
-let test2 = 123;
-let pi = 3.14;
-let age = 16;
-
-
-console.log(day);
-console.log(mounth);
-console.log(num);
-console.log(test2);
-console.log(pi);
-console.log(age);
-
-alert(day);
-alert(mounth);
-alert(num);
-alert(test2);
-alert(pi);
-alert(age);
-
-document.write(day +'<br>');
-document.write(mounth +'<br>');
-document.write(num +'<br>');
-document.write(test2 +'<br>');
-document.write(pi +'<br>');
-document.write(age +'<br>');
-
-let isYes = true;
-let isNo= false;
-
-console.log(isYes);
-console.log(isNo);
-
-alert(isYes);
-alert(isNo);
-
-document.write(isYes +'<br>');
-document.write(isNo +'<br>');
-
-
-
-
-
-
-// task 2
-
-mess = 'goodbye';
-test1 = 'opppsss';
-web = 'org';
-country = 'ru';
-
-console .log(mess);
-console .log(test1);
-console .log(web);
-console.log(country);
-
-alert(mess);
-alert(test1);
-alert(web);
-alert(country);
-
-document.write(mess);
-document.write(test1);
-document.write(web);
-document.write(country);
-
-day = 5;
-mounth = 12;
-num = -888;
-test2 = 321;
-pi = 3.141264634;
-age = 18;
-
-console.log(day);
-console.log(mounth);
-console.log(num);
-console.log(test2);
-console.log(pi);
-console.log(age);
-
-alert(day);
-alert(mounth);
-alert(num);
-alert(test2);
-alert(pi);
-alert(age);
-
-document.write(day);
-document.write(mounth);
-document.write(num);
-document.write(test2);
-document.write(pi);
-document.write(age);
-
-isYes = false;
-isNo= true;
-
-console.log(isYes);
-console.log(isNo);
-
-alert(isYes);
-alert(isNo);
-
-document.write(isYes)
-document.write(isNo);
-
-// task 3
-
-const  user = 'Bob';
-const adress = 'Khakiv';
-const  user2 = 'Bill';
-
-const width = 45;
-const sizeLeg = 41;
-const height = 174;
-
-console.log(user);
-console.log(user2);
-console.log(adress);
-console.log(width);
-console.log(sizeLeg);
-console.log(height);
-
-alert(user);
-alert(user2);
-alert(adress);
-alert(sizeLeg);
-alert(width);
-alert(height);
-
-document.write(user);
-document.write(user2);
-document.write(adress);
-document.write(sizeLeg);
-document.write(width);
-document.write(height);
-
-// task 4
-let firstName = prompt('Enter you name', '');
-let  lastName = prompt('Enter you last name', '');
-let middleName = prompt('Enter you middle name', '');
-
-console.log(firstName);
-console.log(lastName);
-console.log(middleName);
-
-alert(firstName);
-alert(lastName);
-alert(middleName);
-
-document.write(firstName);
-document.write(lastName);
-document.write(middleName);
-
-// task 5
-
-let person = (firstName + ' ' + lastName + ' ' + middleName);
-
-alert(person);
-
-console.log(person);
-
-document.write(person);
-
-// task 6
-// myHusband
-
-let husbandFirstName = prompt('Enter husban name', '');
-let husbandLastName = prompt('Enter husband last name');
-let husbandMiddleName = prompt('Enter husband middle name');
-
-console.log(husbandFirstName);
-console.log(husbandLastName);
-console.log(husbandMiddleName);
-
-alert(husbandFirstName);
-alert(husbandLastName);
-alert(husbandMiddleName);
-
-document.write(husbandFirstName);
-document.write(husbandLastName);
-document.write(husbandMiddleName);
-
-// myDaughter
-
-let personHusband =(husbandFirstName + ' ' + husbandLastName + ' ' + husbandMiddleName);
-
-console.log(personHusband);
-
-alert(personHusband);
-
-document.write(personHusband);
-
-let daughterFirstName = prompt('Enter daughter name', '');
-let daughterLastName = prompt('Enter  daughter last name');
-let daughterMiddleName = prompt('Enter  daughter middle name');
-
-console.log(daughterFirstName);
-console.log(daughterLastName);
-console.log(daughterMiddleName);
-
-alert(daughterFirstName);
-alert(daughterLastName);
-alert(daughterMiddleName);
-
-document.write(daughterFirstName);
-document.write(daughterLastName);
-document.write(daughterMiddleName);
-
-let personDaughter = (daughterFirstName + ' ' + daughterLastName + ' ' + daughterMiddleName);
-
-console.log(personDaughter);
-alert(personDaughter);
-document.write(personDaughter);
-
- // task7
-
-let num1 = +prompt('Enter first number' ,'');
-let num2 = +prompt('Enter second number', '');
-let num3 = +prompt('Enter third number', '');
-
-console.log(num1);
-console.log(num2);
-console.log(num3);
-
-// task 8
-// parseInt()
-
-let num4 = prompt('Enter first number' ,'');
-let num5 = prompt('Enter second number', '');
-let num6 = prompt('Enter third number', '');
-let num7 = prompt('Enter fourth number' ,'');
-
-console.log(parseInt(num4));
-console.log(parseInt(num5));
-console.log(parseInt(num6));
-console.log(parseInt(num7));
-
-let resultTask8 = (parseInt(num4) + parseInt(num5) +parseInt(num6) + parseInt(num7));
-
-console.log(resultTask8);
-
-// task 9
-// parseFloat()
-
-let num8 = prompt('Enter first number' ,'');
-let num9 = prompt('Enter second number', '');
-let num10 = prompt('Enter third number', '');
-
-console.log(parseFloat(num8));
-console.log(parseFloat(num9));
-console.log(parseFloat(num10));
-
-let resultTask9 = (parseFloat(num8)+ parseFloat(num9)+ parseFloat(num10))
-
-console.log(resultTask9);
-
-// task 10
-// Math.round
-
-let num11 = prompt('Enter first number' ,'');
-let num12 = prompt('Enter second number', '');
-let num13 = prompt('Enter third number', '');
-
-console.log(Math.round(num11));
-console.log(Math.round(num12));
-console.log(Math.round(num13));
-
-let resultTask10 = (Math.round(num11) + Math.round(num12) + Math.round(num13));
-
-console.log(resultTask10);
-
-// task 11
-// Math.pow()
-
-let num14 = prompt('Enter first number' ,'');
-let num15 = prompt('Enter second number', '');
-
-
-let result11 = Math.pow(parseInt(num14), parseInt(num15));
-
-console.log(result11);
-
-// task 12
-// typeof
-
-let a = 100;
-let b = '100';
-let c = true;
-let d = undefined;
-
-console.log(typeof a);
-console.log(typeof b);
-console.log(typeof c);
-console.log(typeof d);
-
-// task 13
-
-let aTask13 = 5 < 6;
-console.log(aTask13);
-// -> true
-
-let bTask13 = 5 == 6;
-console.log(bTask13);
-// -> false
-
-let cTask13 = 5 > 6;
-console.log(cTask13);
-// -> false
-
-let dTask13 = 5 >= 6;
-console.log(dTask13);
-// -> false
-
-let eTask13 = 10 == 10;
-console.log(eTask13);
-// -> true
-
-let fTask13 = 10 === 10;
-console.log(fTask13);
-// -> true
-
-let gTask = 10 < 10;
-console.log(gTask);
-// -> false
-
-let kTask13 = 10 != 10;
-console.log(kTask13);
-// > false
-
-let nTask13 = 10 > 10;
-console.log(nTask13);
-// -> false
-
-let  mTask = 123 === '123';
-console.log(mTask);
-// -> false
-
-let lTasr = 123 !== '123';
-console.log(lTasr);
-// -> true
-
+// 'use strict'
+//
+//
+// // task 1
+// let mess = 'hello';
+// let test1 = 'owu';
+// let web = 'com';
+// let country = 'ua';
+//
+// console.log(mess);
+// console.log(test1);
+// console.log(web);
+// console.log(country);
+//
+// alert(mess);
+// alert(test1);
+// alert(web);
+// alert(country);
+//
+// document.write(mess + '<br>');
+// document.write(test1 + '<br>');
+// document.write(web + '<br>');
+// document.write(country +'<br>');
+//
+//
+// let day = 1;
+// let mounth = 10;
+// let num = -999;
+// let test2 = 123;
+// let pi = 3.14;
+// let age = 16;
+//
+//
+// console.log(day);
+// console.log(mounth);
+// console.log(num);
+// console.log(test2);
+// console.log(pi);
+// console.log(age);
+//
+// alert(day);
+// alert(mounth);
+// alert(num);
+// alert(test2);
+// alert(pi);
+// alert(age);
+//
+// document.write(day +'<br>');
+// document.write(mounth +'<br>');
+// document.write(num +'<br>');
+// document.write(test2 +'<br>');
+// document.write(pi +'<br>');
+// document.write(age +'<br>');
+//
+// let isYes = true;
+// let isNo= false;
+//
+// console.log(isYes);
+// console.log(isNo);
+//
+// alert(isYes);
+// alert(isNo);
+//
+// document.write(isYes +'<br>');
+// document.write(isNo +'<br>');
+//
+//
+//
+//
+//
+//
+// // task 2
+//
+// mess = 'goodbye';
+// test1 = 'opppsss';
+// web = 'org';
+// country = 'ru';
+//
+// console .log(mess);
+// console .log(test1);
+// console .log(web);
+// console.log(country);
+//
+// alert(mess);
+// alert(test1);
+// alert(web);
+// alert(country);
+//
+// document.write(mess);
+// document.write(test1);
+// document.write(web);
+// document.write(country);
+//
+// day = 5;
+// mounth = 12;
+// num = -888;
+// test2 = 321;
+// pi = 3.141264634;
+// age = 18;
+//
+// console.log(day);
+// console.log(mounth);
+// console.log(num);
+// console.log(test2);
+// console.log(pi);
+// console.log(age);
+//
+// alert(day);
+// alert(mounth);
+// alert(num);
+// alert(test2);
+// alert(pi);
+// alert(age);
+//
+// document.write(day);
+// document.write(mounth);
+// document.write(num);
+// document.write(test2);
+// document.write(pi);
+// document.write(age);
+//
+// isYes = false;
+// isNo= true;
+//
+// console.log(isYes);
+// console.log(isNo);
+//
+// alert(isYes);
+// alert(isNo);
+//
+// document.write(isYes)
+// document.write(isNo);
+//
+// // task 3
+//
+// const  user = 'Bob';
+// const adress = 'Khakiv';
+// const  user2 = 'Bill';
+//
+// const width = 45;
+// const sizeLeg = 41;
+// const height = 174;
+//
+// console.log(user);
+// console.log(user2);
+// console.log(adress);
+// console.log(width);
+// console.log(sizeLeg);
+// console.log(height);
+//
+// alert(user);
+// alert(user2);
+// alert(adress);
+// alert(sizeLeg);
+// alert(width);
+// alert(height);
+//
+// document.write(user);
+// document.write(user2);
+// document.write(adress);
+// document.write(sizeLeg);
+// document.write(width);
+// document.write(height);
+//
+// // task 4
+// let firstName = prompt('Enter you name', '');
+// let  lastName = prompt('Enter you last name', '');
+// let middleName = prompt('Enter you middle name', '');
+//
+// console.log(firstName);
+// console.log(lastName);
+// console.log(middleName);
+//
+// alert(firstName);
+// alert(lastName);
+// alert(middleName);
+//
+// document.write(firstName);
+// document.write(lastName);
+// document.write(middleName);
+//
+// // task 5
+//
+// let person = (firstName + ' ' + lastName + ' ' + middleName);
+//
+// alert(person);
+//
+// console.log(person);
+//
+// document.write(person);
+//
+// // task 6
+// // myHusband
+//
+// let husbandFirstName = prompt('Enter husban name', '');
+// let husbandLastName = prompt('Enter husband last name');
+// let husbandMiddleName = prompt('Enter husband middle name');
+//
+// console.log(husbandFirstName);
+// console.log(husbandLastName);
+// console.log(husbandMiddleName);
+//
+// alert(husbandFirstName);
+// alert(husbandLastName);
+// alert(husbandMiddleName);
+//
+// document.write(husbandFirstName);
+// document.write(husbandLastName);
+// document.write(husbandMiddleName);
+//
+// // myDaughter
+//
+// let personHusband =(husbandFirstName + ' ' + husbandLastName + ' ' + husbandMiddleName);
+//
+// console.log(personHusband);
+//
+// alert(personHusband);
+//
+// document.write(personHusband);
+//
+// let daughterFirstName = prompt('Enter daughter name', '');
+// let daughterLastName = prompt('Enter  daughter last name');
+// let daughterMiddleName = prompt('Enter  daughter middle name');
+//
+// console.log(daughterFirstName);
+// console.log(daughterLastName);
+// console.log(daughterMiddleName);
+//
+// alert(daughterFirstName);
+// alert(daughterLastName);
+// alert(daughterMiddleName);
+//
+// document.write(daughterFirstName);
+// document.write(daughterLastName);
+// document.write(daughterMiddleName);
+//
+// let personDaughter = (daughterFirstName + ' ' + daughterLastName + ' ' + daughterMiddleName);
+//
+// console.log(personDaughter);
+// alert(personDaughter);
+// document.write(personDaughter);
+//
+//  // task7
+//
+// let num1 = +prompt('Enter first number' ,'');
+// let num2 = +prompt('Enter second number', '');
+// let num3 = +prompt('Enter third number', '');
+//
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+//
+// // task 8
+// // parseInt()
+//
+// let num4 = prompt('Enter first number' ,'');
+// let num5 = prompt('Enter second number', '');
+// let num6 = prompt('Enter third number', '');
+// let num7 = prompt('Enter fourth number' ,'');
+//
+// console.log(parseInt(num4));
+// console.log(parseInt(num5));
+// console.log(parseInt(num6));
+// console.log(parseInt(num7));
+//
+// let resultTask8 = (parseInt(num4) + parseInt(num5) +parseInt(num6) + parseInt(num7));
+//
+// console.log(resultTask8);
+//
+// // task 9s
+// // parseFloat()
+//
+// let num8 = prompt('Enter first number' ,'');
+// let num9 = prompt('Enter second number', '');
+// let num10 = prompt('Enter third number', '');
+//
+// console.log(parseFloat(num8));
+// console.log(parseFloat(num9));
+// console.log(parseFloat(num10));
+//
+// let resultTask9 = (parseFloat(num8)+ parseFloat(num9)+ parseFloat(num10))
+//
+// console.log(resultTask9);
+//
+// // task 10
+// // Math.round
+//
+// let num11 = prompt('Enter first number' ,'');
+// let num12 = prompt('Enter second number', '');
+// let num13 = prompt('Enter third number', '');
+//
+// console.log(Math.round(num11));
+// console.log(Math.round(num12));
+// console.log(Math.round(num13));
+//
+// let resultTask10 = (Math.round(num11) + Math.round(num12) + Math.round(num13));
+//
+// console.log(resultTask10);
+//
+// // task 11
+// // Math.pow()
+//
+// let num14 = prompt('Enter first number' ,'');
+// let num15 = prompt('Enter second number', '');
+//
+//
+// let result11 = Math.pow(parseInt(num14), parseInt(num15));
+//
+// console.log(result11);
+//
+// // task 12
+// // typeof
+//
+// let a = 100;
+// let b = '100';
+// let c = true;
+// let d = undefined;
+//
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+// console.log(typeof d);
+//
+// // task 13
+//
+// let aTask13 = 5 < 6;
+// console.log(aTask13);
+// // -> true
+//
+// let bTask13 = 5 == 6;
+// console.log(bTask13);
+// // -> false
+//
+// let cTask13 = 5 > 6;
+// console.log(cTask13);
+// // -> false
+//
+// let dTask13 = 5 >= 6;
+// console.log(dTask13);
+// // -> false
+//
+// let eTask13 = 10 == 10;
+// console.log(eTask13);
+// // -> true
+//
+// let fTask13 = 10 === 10;
+// console.log(fTask13);
+// // -> true
+//
+// let gTask = 10 < 10;
+// console.log(gTask);
+// // -> false
+//
+// let kTask13 = 10 != 10;
+// console.log(kTask13);
+// // > false
+//
+// let nTask13 = 10 > 10;
+// console.log(nTask13);
+// // -> false
+//
+// let  mTask = 123 === '123';
+// console.log(mTask);
+// // -> false
+//
+// let lTasr = 123 !== '123';
+// console.log(lTasr);
+// // -> true
+//
+// console.log(!!'-1');
+// console.log(!!-1);
+// console.log(!!'0');
+// console.log(!!'null');
+// console.log(!!'undefined');
+// console.log(!!(3/'owu'));
+// console.log((111 > 11 || 45 < 111) ||  !!'0');
+// console.log((!!111 == !!11 || 45 < 111) && (99 > 100 && 45 > 12 ));
---
+++ js/task_Class.js
@@ -0,0 +1,17 @@
+'use strict'
+
+// Class
+//
+// 1.Определите переменные str, num, flag и txt со значениями «Привет», 123, true, «true».
+// При помощи оператора typeof убедитесь, что значения переменных принадлежат типам: string, number и  boolean.
+
+let str = 'Привет';
+let num = 123;
+let flag = true;
+let txt = 'true';
+
+console.log(typeof str);
+console.log(typeof num);
+console.log(typeof flag);
+console.log(typeof txt);
+
